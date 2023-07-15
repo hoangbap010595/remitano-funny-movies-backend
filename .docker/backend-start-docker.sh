e9ba2c58d840eecf3d764b2ab33d31596f412591
hoangbap010595 <lchoang1995@gmail.com>
2023-07-16 03:29:28 +0700

    edit file deploy

--- .docker/backend-start-docker.sh
+++ .docker/backend-start-docker.sh
@@ -7,6 +7,7 @@ docker-compose -f backend-docker-compose.yml -p $APP_NAME down
 sudo rm -rf "$APP_VOLUME_CODE_MOUNT_PATH"
 sudo mkdir -p "$APP_VOLUME_CODE_MOUNT_PATH"
 
+docker-compose -f backend-docker-compose.migrate.yml up --build -d
 docker-compose -f backend-docker-compose.yml -p $APP_NAME up --build -d
 
 echo "===> removing old image"
